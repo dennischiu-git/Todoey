f01cb5ba8d6a2f53dfeee7efdb5827f6ee538c5e
Dennis Chiu <dennischiu@ymail.com>
2019-09-20 00:28:51 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dennis Chiu on 9/19/19.
+//  Copyright © 2019 Dennis Chiu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
